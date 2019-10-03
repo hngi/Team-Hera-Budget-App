82cbab23dabe7d40a4f4e0a9ea29956a346e107c
Abiodun Agbolade <biodun.agbolade@gmail.com>
2019-10-03 22:07:10 +0100

    TO Add Profile Image for Users

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,21 @@
-<?php	
+<?php
 session_start();
+require_once "config/db_config.php";
 
 	if (!(isset($_SESSION['username']) && $_SESSION['username'] != ''))
 	{ // we don't have session at all
 		header("Location:index.html");
 	}
-	
+
 	$user_id = $_SESSION['user_id'];
 	$firstname = $_SESSION['firstname'];
 	$lastname = $_SESSION['lastname'];
 	$username = $_SESSION['username'];
+
+	$sql = "SELECT * FROM profile WHERE userID = $user_id";
+	$row = $dbh->query($sql);
+	$result = mysqli_fetch_assoc($row);
+
 ?>
 
 <!DOCTYPE html>
@@ -45,43 +51,45 @@ session_start();
 
   <body>
     <!--work on the html @message @developerRuq @Goldin @thrive-->
-
     <div class="container-fluid">
       <div class="row">
-        
-
-
         <div class=" budget-board col-md-12">
           <div class="row">
             <div class="col">
-           
-           <div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  Menu
-  </a>
-  <br>
+		<?php
+		$errMsg = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		if (strpos($errMsg, 'uploadsuccess') !== false){
+				echo "<div style='text-align:center; color:white'>Profile Image Successful Uploaded</div>";
+		}
 
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <div class="choked"> <a href="index.html"><img class="budget-logo" src="img/logo normal.png"></a>
-          <br>
-          <br>
-          <ul class="dashboad_nav">
-            <li> <img class="dashboard_icon" src="img/home.svg"> <a class="dashboard_link "  href="index.html"> Home</a></li>
-            <li> <img class="dashboard_icon" src="img/brick.png"> <a class="dashboard_link active"  href=""> Dashboard</a></li>
-            <li> <img class="dashboard_icon" src="img/budg.png"> <a class="dashboard_link" href=""> Budget</a>
-              <ul>
-                <li> <a  class="dashboard_link small small_link" href=""> New Budget</a> </li>
-                <li> <a class="dashboard_link small small_link" href="history.php" > Budget History</a> </li>
-              </ul>
-            </li>
-            <li> <img class="dashboard_icon" src="img/team.svg"> <a class="dashboard_link "  href="team.html"> The Team</a></li>
-            <li> <img class="dashboard_icon" src="img/brick.png"> <a class="dashboard_link "  href="contact.html"> Contact Us</a></li>
-           <li> <img class="dashboard_icon" src="img/logout.png"> <a class="dashboard_link" href="logout.php"> Log Out</a></li>
-          </ul>
-    </div>
-  </div>
-</div>
-              
+		?>
+           <div class="dropdown">
+						  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Menu </a>
+						  <br>
+
+						  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+						    <div class="choked"> <a href="index.html"><img class="budget-logo" src="img/logo normal.png"></a>
+					          <br>
+					          <br>
+					          <ul class="dashboad_nav">
+					            <li> <img class="dashboard_icon" src="img/home.svg"> <a class="dashboard_link "  href="index.html"> Home</a></li>
+					            <li> <img class="dashboard_icon" src="img/brick.png"> <a class="dashboard_link active"  href=""> Dashboard</a></li>
+					            <li> <img class="dashboard_icon" src="img/budg.png"> <a class="dashboard_link" href=""> Budget</a>
+					              <ul>
+					                <li> <a  class="dashboard_link small small_link" href=""> New Budget</a> </li>
+					                <li> <a class="dashboard_link small small_link" href="history.php" > Budget History</a> </li>
+					              </ul>
+					            </li>
+					            <li> <img class="dashboard_icon" src="img/team.svg"> <a class="dashboard_link"  href="team.html"> The Team</a></li>
+											<li data-toggle="modal" data-target="#coolStuffModal" style="cursor:pointer"><img class="dashboard_icon" src="img/team.svg">
+												<a class="dashboard_link" style="">Add Profile image</a></li>
+											<li> <img class="dashboard_icon" src="img/brick.png"> <a class="dashboard_link "  href="contact.html"> Contact Us</a></li>
+					           	<li> <img class="dashboard_icon" src="img/logout.png"> <a class="dashboard_link" href="logout.php"> Log Out</a></li>
+					          </ul>
+						    </div>
+						  </div>
+						</div>
             </div>
           </div>
           <br>
@@ -89,15 +97,14 @@ session_start();
             <div class="row">
             <div class="col-md-10">
             <h3>NEW BUDGET</h3>
-
             </div>
             <div class="col-md-2">
               <div class="name-tag">
-                 <img class="user-img" src="img/profile_logo.jpg"> <?php echo $firstname . ' ' . $lastname ?>
+
+								<img class="user-img" src="<?php echo $result['image']; ?>" class="profileImg" alt=""><?php echo $firstname . ' ' . $lastname ?>
                </div>
             </div>
           </div>
-
          <div class="row budget-panel">
             <div class="col-md-3 col-sm-4 ">
               <div class="card">
@@ -107,14 +114,12 @@ session_start();
                   <div class="row">
                     <div class="col-md-2 offset-1">
                       <img class="naira-symbol" src="img/Naira.png" alt="naira symbol">
-
                     </div>
                     <div class="col-md-8">
-
                       <input type="number" class="form-control"  name="userbudget" id="userbudget" placeholder="E.g 250,000">
                       <span id="addedBudgetResponseMessage"> </span>
                       <span id="budgetResponseMessage" >  </span>
-<br>
+											<br>
                       <br>
                       <button id="budgetButton" type="submit" class="btn btn-primary submit-btn">SUBMIT</button>
                     </div>
@@ -123,24 +128,18 @@ session_start();
                 </form>
               </div>
               <br><br>
-
                <div class="card">
                 <h5>Enter Budget</h5>
                <form id="expenseform" >
                 <div class="form-group">
                   <div class="row">
                     <div class="col-md-10 offset-1">
-
-
                       <input type="text" class="form-control" name="expensename" id="expensename" placeholder="Item">
                       <span id="expenseResponseMessage" >  </span>
                       <span id="addedExpenseResponseMessage"> </span>
                       <br>
                     </div>
-
-
                     </div>
-
                       <div class="form-group">
                         <div class="row">
                         <div class="col-md-5 offset-1">
@@ -156,14 +155,11 @@ session_start();
                                 <option value="5">5</option>
                             </select>
                         </div>
-
-
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-8 offset-2">
                          <button id="expenseButton" class="btn btn-primary submit-btn">SUBMIT</button>
-
                       </div>
                     </div>
                     </div>
@@ -171,15 +167,10 @@ session_start();
                   </div>
                   <br>
                   <br>
-
                    <div>
                          <button id="calculate" class="btn btn-primary submit-btn">CALCULATE BUDGET</button>
-
                   </div>
-
-
                 </div>
-
                 <div class="col-md-9  budget-list-card">
                   <div class="card">
                     <table id="table" class="table">
@@ -189,18 +180,13 @@ session_start();
                           <th scope="col">Item</th>
                           <th scope="col">Priority</th>
                           <th scope="col">Amount</th>
-
                         </tr>
                       </thead>
                       <tbody id="tbody">
-
                       </tbody>
                     </table>
-
-
                   </div>
                   <br><br><br>
-
                    <div>
                     <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary submit-btn" data-toggle="modal" data-target="#exampleModal">
@@ -212,56 +198,75 @@ session_start();
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" >What Do you want to save this as ?</h5>
-        
-      </div>
-      <div class="modal-body">
-         <form>
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-                <div class="form-group">
-
-
-                      <input type="text" class="form-control"  name="budgetTitle" id="budgetTitle" placeholder="Christmas Plan">
-                      <!-- <span id="addedBudgetResponseMessage"> </span>
-                      <span id="budgetResponseMessage" >  </span> -->
-                      <br>
-                      <!-- <button id="saveBudget" type="submit" class="btn btn-primary submit-btn">SUBMIT</button> -->
-                    </div>
-                
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary submit-btn" data-dismiss="modal">Close</button>
-        <button id="saveBudget" type="button" class="btn btn-primary submit-btn">Save changes</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-                         <!-- <button class="btn btn-primary submit-btn">SAVE BUDGET</button> -->
+					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" >What Do you want to save this as ?</h5>
+							   </div>
+							    <div class="modal-body">
+							       <form>
+							         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							          <span aria-hidden="true">&times;</span>
+							       		</button>
+							          <div class="form-group">
+					                 <input type="text" class="form-control"  name="budgetTitle" id="budgetTitle" placeholder="Christmas Plan">
+					                  <!-- <span id="addedBudgetResponseMessage"> </span>
+					                  <span id="budgetResponseMessage" >  </span> -->
+					                  <br>
+					                  <!-- <button id="saveBudget" type="submit" class="btn btn-primary submit-btn">SUBMIT</button> -->
+					               </div>
+									       <div class="modal-footer">
+									        <button type="button" class="btn btn-secondary submit-btn" data-dismiss="modal">Close</button>
+						        			<button id="saveBudget" type="button" class="btn btn-primary submit-btn">Save changes</button>
+						      			 </div>
+						        	</form>
+					 					</div>
+						    </div>
+						  </div>
+						</div>
+   <!-- <button class="btn btn-primary submit-btn">SAVE BUDGET</button> -->
 
                   </div>
-
                 </div>
               </div>
-
-
-
             </div>
           </div>
-
         </div>
-    </div>
+    	</div>
+
+		<!-- Modal for uploading profile image -->
+			<div class="modal fade" id="coolStuffModal" tabindex="-1" role="dialog" aria-labelledby="coolStuffLabel" aria-hidden="true">
+			  <div class="modal-dialog" role="document">
+			    <div class="modal-content" style="text-align:center">
+			      <div class="modal-header">
+			        <h5 class="modal-title" id="contentLabel" style="text-align:center">Kindly Upload Profile image</h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          <span aria-hidden="true">&times;</span>
+			        </button>
+			      </div>
+			      <div class="modal-body">
+			        <div class="container">
+			        	<div class="row">
+					      	<div class="col-sm-12">
+						        <form action="upload.php" method="post" class="form-group" enctype="multipart/form-data">
+											<input type="file" name="image" id="image" class="form-control" value="">
+											<button type="submit" name="submit" class="btn btn-info"style="margin:1rem">Upload Image</button>
+						            <p>Ensure it is either of these format: 'jpg', 'jpeg' 'svg' or 'png'.</p>
+						        </form>
+							    </div>
+					      </div>
+					    </div>
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-secondary" data-dismiss="modal" style="margin: 0px">Close</button>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+
 </div>
-
-
+</div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
