b7f3469e49db2aec0471c29cb959997443858339
dokoede <dennisokoede@outlook.com>
2019-10-04 10:24:22 +0100

    added charts

--- js/budget.js
+++ js/budget.js
@@ -1,7 +1,19 @@
 /* js file for budget page */
-
+window.onload = () => {
+  let editableBudgets = document.getElementsByClassName("editableBudget");
+  console.log(editableBudgets);
+  for (i = 0; i < editableBudgets.length; i++) {
+    editableBudgets.item(i).addEventListener("keyup", event => {
+      if (event.keyCode === 13) {
+        //event.preventDefault();
+        console.log("works!");
+      }
+      console.log("hey");
+    });
+  }
+};
 const addBudgetForm = document.getElementById("budgetform");
-const budgetResponseMessage = document.getElementById("budgetResponseMessage");
+const budgetResponseMessage = document.getElementById("budgetResponseMessage"); //contains please enter a valid budget message
 const expenseResponseMessage = document.getElementById(
   "expenseResponseMessage"
 );
@@ -13,7 +25,7 @@ const addedBudgetResponseMessage = document.getElementById(
 const addExpenseForm = document.querySelector("#expenseform");
 const calculateBtn = document.getElementById("calculate");
 const table = document.getElementById("table");
-const tbody= document.getElementById("tbody");
+const tbody = document.getElementById("tbody");
 
 const expenseArray = [];
 let Budget = {};
@@ -26,7 +38,7 @@ addBudgetForm.addEventListener("submit", event => {
   event.preventDefault();
   userBudget = document.getElementById("userbudget").value;
 
-  if (userBudget < 0 || userBudget === "" || userBudget === null) {
+  if (userBudget <= 0 || userBudget === "" || userBudget === null) {
     budgetResponseMessage.append("Please, enter a valid budget.");
     setTimeout(function() {
       budgetResponseMessage.remove();
@@ -67,16 +79,15 @@ addExpenseForm.addEventListener("submit", event => {
     // Send Success MEssage
     addedExpenseResponseMessage.append(`Added "${expenseName}" to Budget.`);
 
-
     // Render table here
     const tr = document.createElement("tr");
-    
+
     const _id = expenseName.trim().slice(0, 2);
     tr.innerHTML = `
     <td> <span class="budget-icon"> ${_id}  </span> </td>
     <td> ${expenseName}  </td>
     <td> ${priority}  </td>
-    <td> ₦ ...  </td>
+    <td>₦... </td>
     <hr>
     `;
 
@@ -147,7 +158,7 @@ const calculateBudget = async () => {
 calculateBtn.addEventListener("click", calculateBudget);
 
 const renderExpenses = (array, balance) => {
-  console.log(tbody)
+  console.log(tbody);
 
   const thead = `
   <thead  class="thead-light">
@@ -160,9 +171,9 @@ const renderExpenses = (array, balance) => {
                         </tr>
                         
                       </thead>
-  `
+  `;
   table.innerHTML = " ";
-  table.innerHTML = thead
+  table.innerHTML = thead;
   for (expense in array) {
     const tr = document.createElement("tr");
     // let _id = array[expense].expenseName.slice(0 , 1);
@@ -171,9 +182,46 @@ const renderExpenses = (array, balance) => {
     <td> <span class="budget-icon"> ${array[expense]._id}  </span> </td>
     <td> ${array[expense].expenseName}  </td>
     <td> ${array[expense].priority}  </td>
-    <td> ₦ ${array[expense].fundAllocated}  </td>
-    <hr>
+    
     `;
+
+    const newTD = document.createElement("td");
+    newTD.appendChild(document.createTextNode("₦ "));
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("class", "editableBudget");
+    newInput.setAttribute("value", `${array[expense].fundAllocated}`);
+    newInput.setAttribute("keyup", "editBudget()");
+    let originalprice = document.createAttribute("data-originalprice");
+    originalprice.value = `${array[expense].fundAllocated}`;
+    newInput.setAttributeNode(originalprice);
+    newTD.appendChild(newInput);
+    newInput.addEventListener("keyup", editBudget);
+
+    tr.appendChild(newTD);
+
+    hr = document.createElement("hr");
+    tr.appendChild(hr);
+
+    /*let editableBudgets = document.getElementsByClassName("editableBudget");
+    editableBudgets.item(i).addEventListener("keyup",(event) => {
+      if(event.keyCode === 13){
+        //event.preventDefault();
+        console.log("works!");
+        }
+        console.log("hey");
+    });*/
+    //<td> id = "inputHolder"> ₦ <input type = "text" class = "editableBudget" value = "${array[expense].fundAllocated}"></td>
+    //<hr>
+
+    /*const newInput = document.createElement("input");
+    newInput.setAttribute("type","text");
+    newInput.setAttribute("class","editableBudget");
+    newInput.setAttribute("value",`${array[expense].fundAllocated}`);
+    newInput.setAttribute("keyup","editBudget()");
+    .appendChild(newInput);
+    newInput.addEventListener("keyup",editBudget);*/
+
     // console.log(tr);
 
     table.append(tr);
@@ -183,12 +231,13 @@ const renderExpenses = (array, balance) => {
   }
   if (balance) {
     const tr = document.createElement("tr");
+    tr.setAttribute("id", "balanceTR");
 
     tr.innerHTML = `
     <td> </td>
     <td> </td>
     <td> <b> BALANCE </b>  </td>
-    <td> ₦ ${balance}  </td>`;
+    <td >₦ <span id = "balance">${balance}</span></td>`;
 
     table.append(tr);
   } else {
@@ -216,3 +265,120 @@ const roundDown = (num, precision) => {
 // });
 
 //  the code above is for the nav bar
+
+const editBudget = event => {
+  if (event.keyCode === 13) {
+    let originalPrice = parseInt(
+      event.target.dataset.originalprice.replace(/,/gi, "")
+    ); //works
+    let modifiedValue = parseInt(event.target.value.replace(/,/gi, ""));
+    let balance = document.getElementById("balance");
+    /* console.log(originalPrice);
+    console.log(modifiedValue);
+    let balanceValue;*/
+
+    console.log("works!");
+
+    console.log("hey");
+    if (balance === null) {
+      balanceValue = 0;
+      const tr = document.createElement("tr");
+      tr.setAttribute("id", "balanceTR");
+      tr.innerHTML = `
+        <td> </td>
+        <td> </td>
+        <td> <b> BALANCE </b>  </td>
+        <td >₦ <span id = "balance">${balanceValue}</span></td>`;
+
+      table.append(tr);
+    } else {
+      // Do nothing ;
+
+      balanceValue = parseInt(balance.innerHTML); //works
+    }
+
+    //console.log(balanceValue);
+
+    document.getElementById("balance").innerHTML =
+      balanceValue + (originalPrice - modifiedValue);
+    event.target.dataset.originalprice = event.target.value;
+
+    //console.log(typeof document.getElementById("balance").innerHTML);
+  }
+};
+
+const chartfn = function() {
+  const labels = [];
+  const expenseData = [];
+  const color = [];
+
+  for (var i = 0; i < expenseArray.length; i += 1) {
+    labels.push(expenseArray[i].expenseName);
+    color.push("#" + Math.floor(Math.random() * 16777215).toString(16));
+  }
+  // console.log(expenseArray.fundAllocated);
+  for (var i = 0; i < expenseArray.length; i += 1) {
+    expenseData.push(
+      Number(expenseArray[i].fundAllocated.replace(/[^0-9.-]+/g, ""))
+    );
+  }
+
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: "pie",
+
+    // The data for our dataset
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Budget Allocation",
+          backgroundColor: color,
+          borderColor: "#fff",
+          borderWidth: 5,
+          data: expenseData
+        }
+      ]
+    },
+
+    // Configuration options go here
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: "Budget Allocation Chart",
+        fontSize: 15
+      },
+      legend: {
+        position: "bottom",
+        fontSize: "16"
+      },
+      plugins: {
+        datalabels: {
+          color: "#fff",
+          anchor: "end",
+          borderRadius: 25,
+          borderWidth: 2,
+          align: "start",
+          borderColor: "#fff",
+          font: {
+            weight: "bold",
+            size: "10"
+          },
+          formatter: (value, ctx) => {
+            let sum = 0;
+            let dataArr = ctx.chart.data.datasets[0].data;
+            dataArr.map(data => {
+              sum += data;
+            });
+            let percentage = ((value * 100) / sum).toFixed(2) + "%";
+            return percentage;
+          }
+        }
+      }
+    }
+  });
+};
+const dchart = document.querySelector(".dchart");
+dchart.addEventListener("click", chartfn);
